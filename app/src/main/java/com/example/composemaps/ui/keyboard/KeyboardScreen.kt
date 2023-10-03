8ceec6faca8339dd7754d23e66b64e0f4e9fd55b
Lucas Kivi <lucas@livefront.com>
2023-10-03 12:54:55 -0500

    Fix issue where keyboard isn't reopened when text field is focused

--- app/src/main/java/com/example/composemaps/ui/keyboard/KeyboardScreen.kt
+++ app/src/main/java/com/example/composemaps/ui/keyboard/KeyboardScreen.kt
@@ -1,6 +1,5 @@
 package com.example.composemaps.ui.keyboard
 
-import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
@@ -15,31 +14,37 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.focus.onFocusChanged
-import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun KeyboardScreen(
     modifier: Modifier = Modifier,
 ) {
     val focusRequester = remember { FocusRequester() }
     var value by rememberSaveable { mutableStateOf("") }
-    var keyboardShouldBeFocused by rememberSaveable { mutableStateOf(false) }
-    val focusManager = LocalFocusManager.current
+    var keyboardShouldBeOpen by rememberSaveable { mutableStateOf(false) }
+    var textFieldIsFocused by rememberSaveable { mutableStateOf(false) }
+    val textFieldFocusRequester = LocalFocusManager.current
+    val softwareKeyboardController = LocalSoftwareKeyboardController.current
 
-    LaunchedEffect(keyboardShouldBeFocused) {
-        if (keyboardShouldBeFocused) {
-            Log.d("DAVID", "Request focus")
-            focusRequester.requestFocus()
-        } else {
-            focusManager.clearFocus()
-            Log.d("DAVID",  "remove focus")
+    LaunchedEffect(keyboardShouldBeOpen) {
+        when {
+            // Keyboard should be opened but the text field is already in focus.
+            keyboardShouldBeOpen && textFieldIsFocused -> softwareKeyboardController?.show()
+            // Keyboard should be opened and the text field is not focused.
+            keyboardShouldBeOpen -> focusRequester.requestFocus()
+            // Keyboard should be closed and the text field is focused.
+            textFieldIsFocused -> textFieldFocusRequester.clearFocus()
+            // Keyboard should be closed and the text field is focused.
+            else -> softwareKeyboardController?.hide()
         }
     }
 
@@ -56,14 +61,15 @@ fun KeyboardScreen(
                 .onFocusChanged {
                     // If a user taps the text field, focus is gained. This
                     // helps us set `keyboardShouldBeFocused` in that case.
-                    if (keyboardShouldBeFocused.not()) {
-                        keyboardShouldBeFocused = it.isFocused
+                    if (keyboardShouldBeOpen.not()) {
+                        keyboardShouldBeOpen = it.isFocused
                     }
+                    textFieldIsFocused = it.isFocused
                 }
         )
 
         Button(
-            onClick =  { keyboardShouldBeFocused = keyboardShouldBeFocused.not() },
+            onClick =  { keyboardShouldBeOpen = keyboardShouldBeOpen.not() },
             modifier = Modifier.padding(top = 16.dp)
         ) {
             Text("Toggle the keyboard")
